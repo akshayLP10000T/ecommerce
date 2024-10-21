8f510a25c10ee90ab56b9caa8e49a298cc9e4b6f
Akshay Sharma <asharma.19042007@gmail.com>
2024-10-22 00:26:32 +0530

    authentication check middleware

--- backend/middlewares/isAuthenticated.ts
+++ backend/middlewares/isAuthenticated.ts
@@ -11,6 +11,28 @@ declare global{
 
 export const isAuthenticated = async (req: Request, res: Response, next: NextFunction)=>{
     try {
+
+        const token = req.cookies.token;
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "User not authenticated",
+            });
+        }
+
+        const decode = jwt.verify(token, process.env.SECRET_KEY!) as jwt.JwtPayload;
+
+        if(!decode){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid user",
+            })
+        }
+
+        req.id = decode.userId;
+
+        next();
         
     } catch (error) {
         return res.status(500).json({
